2de363bef139b0c1cb4ccf0ee371d17648d5ac7b
Maria Junqueira <maria.junqueira@pravaler.com.br>
2020-08-26 13:03:56 -0300

    Adição do component de course level form no product module

--- src/app/pages/product/product.module.ts
+++ src/app/pages/product/product.module.ts
@@ -1,12 +1,16 @@
+import { CourseLevelFormComponent } from './course-level-form/course-level-form.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { ProductFormComponent } from './product-form/product-form.component';
 import { ProductContractFormComponent } from './product-contract-form/product-contract-form.component';
 
 @NgModule({
-  declarations: [ProductFormComponent, ProductContractFormComponent],
+  declarations: [
+    ProductFormComponent,
+    ProductContractFormComponent,
+    CourseLevelFormComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
@@ -14,7 +18,8 @@ import { ProductContractFormComponent } from './product-contract-form/product-co
   ],
   exports: [
     ProductFormComponent,
-    ProductContractFormComponent
+    ProductContractFormComponent,
+    CourseLevelFormComponent
   ]
 })
 export class ProductModule { }
