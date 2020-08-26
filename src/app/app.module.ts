715aa5fc9e76adaf9ff85134c70dc523d9a3e84d
Maria Junqueira <maria.junqueira@pravaler.com.br>
2020-08-26 11:13:37 -0300

    Adição da biblioteca de InMemoryWebApi no app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
+import { InMemoryDataBase } from './in-memory-database';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PagesModule } from './pages/pages.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-    PagesModule
+    PagesModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBase)
   ],
   providers: [],
   bootstrap: [AppComponent]
