15e59e498a837f66546dbaa920175a4e0160973d
Maria Junqueira <maria.junqueira@pravaler.com.br>
2020-08-26 11:14:58 -0300

    Service de in memory database com os dados de produto teaching modality e courselevel

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,90 @@
+import { CourseLevel } from './pages/product/shared/model/course-level.model';
+import { TeachingModality } from './pages/product/shared/model/teaching-modality.model';
+import { InMemoryDbService } from "angular-in-memory-web-api"
+import { Product } from "./pages/product/shared/model/product.model";
+
+export class InMemoryDataBase implements InMemoryDbService {
+  createDb() {
+    const product: Product =
+    {
+      id: 1192,
+      name: "DSADASDSA",
+      description: "dsadsadas",
+      active: true,
+      courseLevel: [
+        {
+          id: 1,
+          name: "GRADUAÇÃO",
+          active: true,
+        }
+      ],
+      teachingModality: [
+        {
+          id: 3,
+          name: "EAD",
+          active: true,
+        }
+      ],
+      productMultiplierMonthlyPayment: [
+        {
+          id: 3721,
+          multiplierParcels: null,
+          contractInstallments: 2,
+          hiringNumber: 1,
+          guarantorStudentIncome: "2.00"
+        }
+      ],
+      productContract: {
+        id: 1384,
+        parcelsTaxRegister: 2,
+        hasTaxRegister: true,
+        hasFpd: true,
+        firstHiringParts: [
+          {
+            id: 238,
+            monthlyPaymentContract: "2.00",
+            parcelsContract: 2,
+            parcelsTransferNextContract: 1
+          }
+        ]
+      }
+    };
+
+    const teachingModality: Array<TeachingModality> = [
+      {
+        id: 1,
+        name: "Presencial",
+        active: true,
+      }, {
+        id: 2,
+        name: "EAD",
+        active: true,
+      }, {
+        id: 3,
+        name: "Semi-presencial",
+        active: true,
+      }
+    ];
+
+    const courseLevel: Array<CourseLevel> = [
+      {
+        id: 1,
+        name: "Graduação",
+        active: true,
+      }, {
+        id: 2,
+        name: "Pós-graduação",
+        active: true,
+      }, {
+        id: 3,
+        name: "Técnico",
+        active: true,
+      }, {
+        id: 4,
+        name: "Intercâmbio",
+        active: true
+      }
+    ];
+    return { product, teachingModality, courseLevel }
+  }
+}
