b4a1676ee223e8876da8b60cdba887318000bb41
Maria Junqueira <maria.junqueira@pravaler.com.br>
2020-08-26 18:53:55 -0300

    Adição das rotas de criação e edição do primeiro step do cadastro de produtos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
+import { AppComponent } from './app.component';
+import { ProductFormComponent } from './pages/product/product-form/product-form.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PagesComponent } from "./pages/pages.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: PagesComponent,
+    children: [
+      {
+        path: "product/:id",
+        component: ProductFormComponent
+      },
+      {
+        path: "product",
+        component: ProductFormComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
