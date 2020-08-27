4e22e6c02dcc433437c007c1082b14d1692843d2
Maria Junqueira <maria.junqueira@pravaler.com.br>
2020-08-27 02:24:21 -0300

    Directive para habilitar ou desabilitar um campo do formulário

---
+++ src/app/pages/product/shared/directive/disable-control.directive.ts
@@ -0,0 +1,30 @@
+import { NgControl } from '@angular/forms';
+import { Directive, Input, OnInit } from "@angular/core";
+
+@Directive({
+  selector: '[disableControl]'
+})
+export class DisableControlDirective implements OnInit {
+  @Input() set disableControl(condition: boolean) {
+    this.toggleForm(condition);
+    this.disabled = condition;
+  }
+
+  disabled: boolean = false;
+
+  constructor(private ngControl: NgControl) {}
+
+  ngOnInit() {
+    if(this.ngControl) {
+      this.toggleForm(this.disabled);
+    }
+  }
+
+  toggleForm(condition: boolean) {
+    if(condition) {
+      this.ngControl?.control?.disable();
+    } else {
+      this.ngControl?.control?.enable();
+    }
+  }
+}
