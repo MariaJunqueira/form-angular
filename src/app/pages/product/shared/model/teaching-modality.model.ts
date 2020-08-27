e0ce1ee96b440d7dc29f20230c465900a22747d5
Maria Junqueira <maria.junqueira@pravaler.com.br>
2020-08-27 00:16:42 -0300

    Adição de scontructor na model d teachingModality

--- src/app/pages/product/shared/model/teaching-modality.model.ts
+++ src/app/pages/product/shared/model/teaching-modality.model.ts
@@ -1,5 +1,12 @@
 export class TeachingModality {
-  id: number;
+  id?: number;
   name: string;
   active: boolean;
+  selected?: boolean;
+
+  constructor(teachingModality: TeachingModality) {
+    this.id = teachingModality.id;
+    this.name = teachingModality.name;
+    this.active = teachingModality.active;
+  }
 }
