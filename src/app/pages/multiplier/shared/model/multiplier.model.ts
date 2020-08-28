3e4e3bca01882bddc0a156454d89ad977c88f966
Maria Junqueira <maria.junqueira@pravaler.com.br>
2020-08-28 16:33:36 -0300

    Adição de constructor no multiplier model

--- src/app/pages/multiplier/shared/model/multiplier.model.ts
+++ src/app/pages/multiplier/shared/model/multiplier.model.ts
@@ -4,4 +4,12 @@ export class Multiplier {
   contractInstallments: number;
   multiplierParcels: number;
   guarantorStudentIncome: string;
+
+  constructor(object) {
+    this.id = object.id;
+    this.hiringNumber = object.hiringNumber;
+    this.contractInstallments = object.contractInstallments;
+    this.multiplierParcels = object.multiplierParcels || null;
+    this.guarantorStudentIncome = object.guarantorStudentIncome;
+  }
 }
