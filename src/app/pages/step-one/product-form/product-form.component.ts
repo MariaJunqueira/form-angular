93eae96e97847e1e9fea2f8f7124ab3a0d9cd2a9
Maria Junqueira <maria.junqueira@pravaler.com.br>
2020-08-31 18:26:55 -0300

    Adição validadores do campo name do product-form

--- src/app/pages/step-one/product-form/product-form.component.ts
+++ src/app/pages/step-one/product-form/product-form.component.ts
@@ -1,6 +1,8 @@
+import { catchError, map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 import { ProductService } from './../shared/service/product.service';
 import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl, AsyncValidatorFn } from '@angular/forms';
 import { Product } from "../shared/model/product.model";
 import { LoadingService } from 'src/app/shared/components/loading/loading.service';
 
@@ -16,7 +18,8 @@ export class ProductFormComponent implements OnInit {
   public loading = true;
 
   constructor(
-    private loadingService: LoadingService
+    private loadingService: LoadingService,
+    private productService: ProductService
   ) { }
 
   ngOnInit(): void {
@@ -26,9 +29,38 @@ export class ProductFormComponent implements OnInit {
 
   public createForm() {
     this.productForm.setControl('id', new FormControl(this.product?.id || null));
-    this.productForm.setControl('name', new FormControl(this.product?.name || ''));
+    this.productForm.setControl('name', new FormControl(this.product?.name || '', { validators: [Validators.required, this.validateSpaceOnly], asyncValidators: [this.existingNameValidator()], updateOn: "blur"}));
     this.productForm.setControl('description', new FormControl(this.product?.description || ''));
     this.loading = false;
     this.loadingService.decreaseLoader()
+  }
+
+  validateSpaceOnly(control: AbstractControl): { [key: string]: boolean } | null {
+    if (!control.value.replace(/\s/g, '').length) {
+      return { "spaceOnly": true }
+    }
+
+    return null
+  }
+
+  existingNameValidator(): AsyncValidatorFn {
+    return (
+      control: AbstractControl
+    ):
+      | Promise<{ [key: string]: any } | null>
+      | Observable<{ [key: string]: any } | null> => {
+      if (!control.value) {
+        return of(null);
+      } else {
+        return this.productService.getByName({ name: control.value }).pipe(
+          map((product: any) => {
+            return product.content?.id.toString() !== this.product?.id?.toString() ? { "nameExists": { value: true } } : null
+          }),
+          catchError(() => {
+            return of (null);
+          })
+        );
+      }
+    };
   }
 }
