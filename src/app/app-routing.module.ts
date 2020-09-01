903c990e8eba2f27905c6a362d6222292522ce25
Maria Junqueira <maria.junqueira@pravaler.com.br>
2020-09-01 12:21:28 -0300

    Adição de rota para login e authguard nas rotas protegidas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
+import { AuthenticationGuard } from './authentication/shared/service/authenticator.guard';
+import { LoginComponent } from './authentication/login/login.component';
 import { StepOneComponent } from './pages/step-one/step-one.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PagesComponent } from "./pages/pages.component";
+import { AuthenticationComponent } from "./authentication/authentication.component";
 
 const routes: Routes = [
   {
+    path: "login",
+    component: AuthenticationComponent,
+    children: [
+      {
+        path: "",
+        component: LoginComponent
+      }
+    ]
+  }, {
     path: "",
     component: PagesComponent,
+    canActivate: [AuthenticationGuard],
     children: [
       {
         path: "product/:id",
@@ -17,8 +30,7 @@ const routes: Routes = [
         component: StepOneComponent
       }
     ]
-  },
-  {
+  }, {
     path: "**",
     component: PagesComponent
   }
